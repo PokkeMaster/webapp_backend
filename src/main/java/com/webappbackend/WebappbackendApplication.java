941e02607713a4ad1613ef134528f008ec8751aa
Kent McDalz <pokkemaster127@gmail.com>
2023-11-30 14:30:22 +0800

    first commit

---
+++ src/main/java/com/webappbackend/WebappbackendApplication.java
@@ -0,0 +1,13 @@
+package com.webappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webappbackend/WebappbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
